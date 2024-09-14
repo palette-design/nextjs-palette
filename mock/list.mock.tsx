92ca66ca15b14ad7dc63f44407a520633d0a6f29
your_username <kohseukim@gmail.com>
2024-09-14 19:54:37 +0800

    fix: bugs

--- mock/list.mock.tsx
+++ mock/list.mock.tsx
@@ -1,25 +1,22 @@
-
-import { PaletteSideListItemType } from "@/components/layouts/HorizontalLayout/interfaces"
-
-export const listDataSource: PaletteSideListItemType[] = [
-    {
-      key: '1',
-      title: 'Arizona Rose',
-      description: 'ADP6PHAH'
-    },
-    {
-      key: '2',
-      title: 'Belle Starr',
-      description: 'BUX5RK5V'
-    },
-    {
-      key: '3',
-      title: 'Cheyenne Chandler',
-      description: 'EMU6GFPW'
-    },
-    {
-      key: '4',
-      title: 'Doc Holliday',
-      description: 'YDR8PTR8'
-    },
-]
+export const listDataSource: any[] = [
+  {
+    key: "1",
+    title: "Arizona Rose",
+    description: "ADP6PHAH",
+  },
+  {
+    key: "2",
+    title: "Belle Starr",
+    description: "BUX5RK5V",
+  },
+  {
+    key: "3",
+    title: "Cheyenne Chandler",
+    description: "EMU6GFPW",
+  },
+  {
+    key: "4",
+    title: "Doc Holliday",
+    description: "YDR8PTR8",
+  },
+];
